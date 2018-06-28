ed90c1cb6c12640e7232c2163d18477e4e7d8fbb
Frederic Brodbeck <freder.code@gmail.com>
2018-06-28 19:55:13 +0200

    switched to airtable api

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
-const csvtojson = require('csvtojson');
 const R = require('ramda');
+const Airtable = require('airtable');
 
 
 const args = R.drop(2, process.argv);
-const csvFile = R.head(args);
+const apiKey = R.head(args);
+
+
+let allRecords = [];
+const base = new Airtable({ apiKey }).base('appWdn4YdV4o7BEDL');
+base('data')
+	.select({ view: 'Grid view' })
+	.eachPage(
+		(records, fetchNextPage) => {
+			allRecords = [...allRecords, ...records];
+			fetchNextPage();
+		}, 
+
+		// done
+		(err) => {
+			if (err) { 
+				console.error(err); 
+				return; 
+			}
+			
+			const data = R.map(
+				R.pipe(
+					R.prop('fields'),
+					prepare,
+				)
+			)(allRecords);
+			console.log(
+				JSON.stringify(data, null, '\t')
+			);
+		}
+	);
 
 
 const prepare = R.pipe(
 	// prepare data
 	R.evolve({
-		public: (p) => (p === 'checked'),
 		'lap distance (m)': (d) => parseInt(d, 10),
-		'lat lng': R.pipe(
+		'lat / lng': R.pipe(
 			R.split(','), 
 			R.map(R.trim)
 		),
@@ -20,24 +49,15 @@ const prepare = R.pipe(
 	// rename fields
 	(item) => {
 		return R.pipe(
-			R.assoc('latLng', item['lat lng']),
+			R.assoc('latLng', item['lat / lng']),
 			R.assoc('googleMaps', item['google maps']),
 			R.assoc('lapDistance', item['lap distance (m)']),
+			R.assoc('public', item['public'] || false),
 		)(item);
 	},
 	R.omit([
-		'lat lng',
+		'lat / lng',
 		'google maps',
 		'lap distance (m)',
 	]),
 );
-
-
-csvtojson()
-	.fromFile(csvFile)
-	.then(R.map(prepare))
-	.then((data) => {
-		console.log(
-			JSON.stringify(data, null, '\t')
-		);
-	});
